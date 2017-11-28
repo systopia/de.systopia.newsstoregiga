01d4c2e289dba488125f654591da8b687918e946
Nikolai Röhl <nikolai.roehl@giga-hamburg.de>
2017-11-28 13:17:29 +0100

    Adding hardcoded family names and one subject correction

--- CRM/NewsstoreMailer/GigaJournalFamily.php
+++ CRM/NewsstoreMailer/GigaJournalFamily.php
@@ -16,49 +16,49 @@ class CRM_NewsstoreMailer_GigaJournalFamily extends CRM_NewsstoreMailer_GigaComm
       'body_template' => 'journal-family-body-de.html',
       'header'        => 'journalfamily.jpg',
       'subject'       => 'Neue Ausgabe | Africa Spectrum',
-      'family'        => '', // @todo
+      'family'        => 'AFRICA SPECTRUM',
     ],
     'africa-spectrum-en' => [
       'item_template' => 'journal-family-item.html',
       'body_template' => 'journal-family-body-en.html',
       'header'        => 'journalfamily.jpg',
       'subject'       => 'New Issue | Africa Spectrum',
-      'family'        => '', // @todo
+      'family'        => 'AFRICA SPECTRUM', 
     ],
     'chinese-affairs-de' => [
       'item_template' => 'journal-family-item.html',
       'body_template' => 'journal-family-body-de.html',
       'header'        => 'journalfamily.jpg',
-      'subject'       => 'Neue Ausgabe | Journal of Current Southeast Asian Affairs',
-      'family'        => '', // @todo
+      'subject'       => 'Neue Ausgabe | Journal of Current Chinese Affairs',
+      'family'        => 'JOURNAL OF CURRENT CHINESE AFFAIRS',
     ],
     'chinese-affairs-en' => [
       'item_template' => 'journal-family-item.html',
       'body_template' => 'journal-family-body-en.html',
       'header'        => 'journalfamily.jpg',
-      'subject'       => 'New Issue | Journal of Current Southeast Asian Affairs',
-      'family'        => '', // @todo
+      'subject'       => 'New Issue | Journal of Current Chinese Affairs',
+      'family'        => 'JOURNAL OF CURRENT CHINESE AFFAIRS',
     ],
     'latin-america-de' => [
       'item_template' => 'journal-family-item.html',
       'body_template' => 'journal-family-body-en.html',
       'header'        => 'journalfamily.jpg',
       'subject'       => 'Neue Ausgabe | Journal of Politics in Latin America',
-      'family'        => '', // @todo
+      'family'        => 'JOURNAL OF POLITICS IN LATIN AMERICA', 
     ],
     'latin-america-en' => [
       'item_template' => 'journal-family-item.html',
       'body_template' => 'journal-family-body-en.html',
       'header'        => 'journalfamily.jpg',
       'subject'       => 'New Issue | Journal of Politics in Latin America',
-      'family'        => '', // @todo
+      'family'        => 'JOURNAL OF POLITICS IN LATIN AMERICA',
     ],
     'se-asia-de' => [
       'item_template' => 'journal-family-item.html',
       'body_template' => 'journal-family-body-en.html',
       'header'        => 'journalfamily.jpg',
       'subject'       => 'Neue Ausgabe | Journal of Current Southeast Asian Affairs',
-      'family'        => 'JOURNAL OF CURRENT SOUTHEAST ASIAN AFFAIRS', // @todo
+      'family'        => 'JOURNAL OF CURRENT SOUTHEAST ASIAN AFFAIRS',
     ],
     'se-asia-en' => [
       'item_template' => 'journal-family-item.html',
